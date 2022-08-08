3298f904d943b1047aeadabb90e2b745f419e2ab
Kevin Rodriguez <kevinzuelo@yahoo.com>
2022-08-08 13:04:04 -0400

    Added Models for Pokemon and Collection

---
+++ java/src/main/java/com/techelevator/model/Pokemon.java
@@ -0,0 +1,97 @@
+package com.techelevator.model;
+
+public class Pokemon {
+
+    private int pokemonId;
+    private int collectionId;
+    private String name;
+    private String species;
+    private String type;
+    private int level;
+    private boolean isShiny;
+    private String notes;
+    private String imgMain;
+    private String imgSprite;
+
+    public Pokemon() { }
+
+    public int getPokemonId() {
+        return pokemonId;
+    }
+
+    public void setPokemonId(int pokemonId) {
+        this.pokemonId = pokemonId;
+    }
+
+    public int getCollectionId() {
+        return collectionId;
+    }
+
+    public void setCollectionId(int collectionId) {
+        this.collectionId = collectionId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public boolean isShiny() {
+        return isShiny;
+    }
+
+    public void setShiny(boolean shiny) {
+        isShiny = shiny;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getImgMain() {
+        return imgMain;
+    }
+
+    public void setImgMain(String imgMain) {
+        this.imgMain = imgMain;
+    }
+
+    public String getImgSprite() {
+        return imgSprite;
+    }
+
+    public void setImgSprite(String imgSprite) {
+        this.imgSprite = imgSprite;
+    }
+}
