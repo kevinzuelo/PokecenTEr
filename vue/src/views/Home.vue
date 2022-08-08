edfd38a7feb28ee896517d5e4b17d5f766f6b35a
Alex Roffey <alexandra.roffey@gmail.com>
2022-08-08 18:01:03 -0400

    working on Collections Preview component

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <h1>My Collections</h1>
+  
+    <collection-preview />
+
+    <h1>Browse All Collections</h1>
   </div>
 </template>
 
 <script>
+import CollectionPreview from '../components/CollectionPreview.vue';
+
 export default {
+  components: { CollectionPreview },
   name: "home"
 };
 </script>
+
+<style scoped>
+
+h1 {
+  color: white;
+}
+
+</style>
