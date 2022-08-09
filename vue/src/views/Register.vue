bebf659be74a8694f0d00ee2b771476a7f4a70df
Alex Roffey <alexandra.roffey@gmail.com>
2022-08-09 15:31:39 -0400

    added blurb to landing page and image to register page

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="register" class="text-center">
+    <img src="..\images\Pokecenter-logo.png" class="top-img"/>
     <form class="form-register" @submit.prevent="register">
       <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
       <div class="alert alert-danger" role="alert" v-if="registrationErrors">
@@ -210,4 +211,10 @@ label {
   flex-direction: column;
   align-items: center;
 }
+  .top-img {
+    max-height: 10%;
+    position: static;
+    top: 30px;
+    margin: 30px;
+  }
 </style>
