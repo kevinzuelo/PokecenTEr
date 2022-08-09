b5361d05bb9e78c592e1d4774abca29d93776a0c
Kevin Rodriguez <kevinzuelo@yahoo.com>
2022-08-09 13:36:37 -0400

    Displays user's collections

--- vue/src/services/CollectionService.js
+++ vue/src/services/CollectionService.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 
 export default {
 
-
-
   createCollection(newCollection) {
     return axios.post('/collections', newCollection)
+  },
+
+  getCollectionsByUserId(userId) {
+    return axios.get(`/user/${userId}/collections`)
   }
 
 }
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h1>My Collections</h1>
     <div class="myCollections">
-    <collection-preview />
+    <collection-preview v-for="collection in collections" v-bind:key="collection.id" v-bind:collection="collection"/>
     <add-new-collection />
     </div>
     <h1>Browse All Collections</h1>
@@ -12,10 +12,22 @@
 <script>
 import CollectionPreview from '../components/CollectionPreview.vue';
 import AddNewCollection from '../components/AddNewCollection.vue';
+import CollectionService from '../services/CollectionService'
 
 export default {
-  components: { CollectionPreview, AddNewCollection },
-  name: "home"
+  data() {
+    return {
+     collections : []
+    }
+  },
+  components: { CollectionPreview, AddNewCollection},
+  name: "home",
+  created() {
+    CollectionService.getCollectionsByUserId(5).then((response) => {
+      this.collections = response.data;
+      console.log(response.data)
+    })
+  }
 };
 </script>
 
