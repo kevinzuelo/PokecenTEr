72558e33437261b6a2d412b9983e25eec7a7b796
Donald Kiser <dkiser85@yahoo.com>
2022-08-09 11:38:32 -0400

    addnewcollection

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <h1>My Collections</h1>
-  
+    <div class="myCollections">
     <collection-preview />
-
+    <add-new-collection />
+    </div>
     <h1>Browse All Collections</h1>
   </div>
 </template>
 
 <script>
 import CollectionPreview from '../components/CollectionPreview.vue';
+import AddNewCollection from '../components/AddNewCollection.vue';
 
 export default {
-  components: { CollectionPreview },
+  components: { CollectionPreview, AddNewCollection },
   name: "home"
 };
 </script>
@@ -22,5 +24,11 @@ export default {
 h1 {
   color: white;
 }
+.myCollections {
+  display: flex;
+  gap: 25px;
+  align-items: center;
+}
+
 
 </style>
