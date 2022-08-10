bba99359d436f70a9cef29a639b75a31bb5a2d51
Thomas Blatnik <thomasblatnik@gmail.com>
2022-08-10 11:30:45 -0400

    Created PokemonPreview component to display in collection

---
+++ vue/src/components/PokemonPreview.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="pokemon-preview">
+        <h3>{{pokemon.name}}</h3>
+        <h4>Lvl {{pokemon.level}}</h4>
+        <img v-bind:src=pokemon.imgSprite />
+        <p>{{pokemon.type}}</p>   
+    </div>
+</template>
+<script>
+export default {
+  name: "pokemon-preview",
+  props: ["pokemon"],
+  methods: {
+    
+    }
+  }
+
+
+</script>
+
+<style>
+  .pokemon-preview {
+    display: inline-block;
+    background-color: lightskyblue;
+    padding: 10px;
+    text-align: center;
+  }
+</style>
