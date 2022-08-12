2d16341da35f3e8f36364de29e35c12a71b60bec
Colin McGinty <cmcginty13@yahoo.com>
2022-08-12 11:32:05 -0400

    fixed the race condition in display collection statistics

--- vue/src/components/DisplayCollectionStatistics.vue
+++ vue/src/components/DisplayCollectionStatistics.vue
@@ -1,7 +1,8 @@
 <template>
-  <div id="statistics-container">
-    <div id="individual-statistic" v-for="(n,index) in this.typeList.length" v-bind:key="index">
-      <h5>{{ typeList[index] }} : {{ stats[typeList[index]] }}</h5>
+  <div v-if="isLoaded" id="statistics-container">
+    <div id="individual-statistic" v-for="(n,index) in typeList.length" v-bind:key="index">
+      <h5>{{ stats[typeList[index]] }}</h5>
+      <img :src="images[index]" width="50px" />
     </div>
   </div>
 </template>
@@ -13,33 +14,19 @@ export default {
   data(){
     return{
       typeList: ["bug","dark","dragon","electric","fairy","fighting","fire","flying","ghost","grass","ground","ice","normal","poison","psychic","rock","steel","water"],
-      images: {
-        "bug": "https://static.wikia.nocookie.net/pokemon/images/6/64/Type_Bug.gif",
-        "dark": "https://static.wikia.nocookie.net/pokemon/images/0/0d/Type_Dark.gif",
-        "dragon": "https://static.wikia.nocookie.net/pokemon/images/2/26/Type_Dragon.gif",
-        "electric": "https://static.wikia.nocookie.net/pokemon/images/a/aa/Type_Electric.gif",
-        "fairy": "https://static.wikia.nocookie.net/pokemon/images/7/74/Type_Fairy.gif",
-        "fighting": "https://static.wikia.nocookie.net/pokemon/images/6/6b/Type_Fighting.gif",
-        "fire": "https://static.wikia.nocookie.net/pokemon/images/4/4d/Type_Fire.gif",
-        "flying": "https://static.wikia.nocookie.net/pokemon/images/4/4b/Type_Flying.gif",
-        "ghost": "https://static.wikia.nocookie.net/pokemon/images/f/f2/Type_Ghost.gif",
-        "grass": "https://static.wikia.nocookie.net/pokemon/images/4/46/Type_Grass.gif",
-        "ground": "https://static.wikia.nocookie.net/pokemon/images/1/1d/Type_Ground.gif",
-        "ice": "https://static.wikia.nocookie.net/pokemon/images/8/84/Type_Ice.gif",
-        "normal": "https://static.wikia.nocookie.net/pokemon/images/6/61/Type_Normal.gif",
-        "poison": "https://static.wikia.nocookie.net/pokemon/images/8/82/Type_Poison.gif",
-        "psychic" : "https://static.wikia.nocookie.net/pokemon/images/6/65/Type_Psychic.gif",
-        "rock": "https://static.wikia.nocookie.net/pokemon/images/b/b3/Type_Rock.gif",
-        "steel": "https://static.wikia.nocookie.net/pokemon/images/a/ab/Type_Steel.gif",
-        "water": "https://static.wikia.nocookie.net/pokemon/images/e/ed/Type_Water.gif",
-      },
-      stats: {}
+      stats: {},
+      images: [],
+      isLoaded: false
     }
   },
   created(){
     PokemonService.getCollectionTypeStatistics(this.collectionId).then(response => {
       this.stats = response.data;
-    })
+    });
+    for(let i = 0; i<this.typeList.length; i++){
+      this.images.push(this.$store.state.images[this.typeList[i]])
+    }
+    this.isLoaded = true;
   },
   props: ["collectionId"]
 }
