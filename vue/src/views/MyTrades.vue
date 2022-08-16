3066548b1873ec134ad83f6503368743927541c4
Alex Roffey <alexandra.roffey@gmail.com>
2022-08-16 14:59:37 -0400

    created trade history component

--- vue/src/views/MyTrades.vue
+++ vue/src/views/MyTrades.vue
@@ -1,11 +1,15 @@
 <template>
+<div>
   <trade-requests />
+  <trade-history /> 
+  </div>
 </template>
 
 <script>
+import TradeHistory from '../components/TradeHistory.vue'
 import TradeRequests from '../components/TradeRequests.vue'
 export default {
-  components: { TradeRequests },
+  components: { TradeRequests, TradeHistory }
 
 }
 </script>
